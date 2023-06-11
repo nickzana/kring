5ea0cfeb9ea12f86bd99f7d90288fdde3585487e
Nick Zana <me@nickzana.dev>
2023-06-10 22:57:38 -0400

    ctap2-proto: Derive common traits for extensions::cred_protect::Policy
    Derive Clone, Copy, and serde traits for usage in CTAP messages

--- crates/ctap2-proto/src/extensions/cred_protect.rs
+++ crates/ctap2-proto/src/extensions/cred_protect.rs
@@ -1,3 +1,6 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum Policy {
     UserVerificationOptional = 0x01,
     UserVerificationOptionalWithCredentialIdList = 0x02,
